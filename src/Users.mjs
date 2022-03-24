5129eb7b5d1c663bfbe01934ab225ccd129322a9
serialized-kirin <67888548+serialized-kirin@users.noreply.github.com>
2022-03-24 10:56:52 -0400

    Create Users.mjs
    made basic minimum interface (everything used in `server.mjs`) for `Users`, `Tokens`, and `generateToken()`.

---
+++ src/Users.mjs
@@ -0,0 +1,35 @@
+import Store from 'Store.mjs';
+
+const db = new Store('db.json', {
+  autoWrite: (1000 * 60), // set the database to write to the file every minute
+});
+
+const Tokens = {
+  tokens: Object.keys(db.cache),
+  valid: function(token) {
+    return Object.keys(db.cache).indexOf(token) !== -1;
+  },
+  add: function(token) {
+    if(Tokens.valid(token) === true) //make sure the token doesn't already exist
+      throw "Invalid Token";
+    Tokens.tokens.push(token);
+  },
+};
+
+function generateToken()
+{
+  const token = Buffer.from(
+    Date.now().toString() + Math.floor(Math.random()*(1 << 20)).toString()
+  ).toString('base64');
+  return token;
+}
+
+const Users = {
+  get: (token) => db.cache[token],
+  set: (token, obj) => Object.assign(db.cache[token], obj),
+};
+
+
+export Users;
+export Tokens;
+export generateToken;
