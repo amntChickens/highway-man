2c3ae6bba8e88fee3c8f87d38ea5c96a69a3a922
amntChickens <67888548+amntChickens@users.noreply.github.com>
2022-03-23 20:07:04 -0400

    Update server.mjs
    I added the 'create' (`post`) part of the api, so now you can actually make a token if you want. 
    
    I also made `Users`, `Tokens`, and `generateToken` less tightly coupled, so I can avoid some headaches later on. 
    
    I made `Tokens` and `Users` a little bit more encapsulated as well, for the same sorts of reasons.

--- src/server.mjs
+++ src/server.mjs
@@ -4,13 +4,14 @@ import {Tokens, generateToken, Users, User} from './Users.mjs'
 
 const api = new JsonAPI({
   token: 'string'
-});
+}, 'users');
 
+// read a resource
 api.get({ resource: 'string' }, ({ token, resource }) => {
-  if(token === 'EMPTY' && resource === 'new token')
-    return { token: generateToken() };
+  if(token === 'EMPTY')
+    return { error: 'empty token' };
   
-  if(Tokens.indexOf(token) === -1)
+  if(Tokens.valid(token) === false)
     return { error: 'invalid token' };
   
   const user = Users.get(token);
@@ -24,4 +25,26 @@ api.get({ resource: 'string' }, ({ token, resource }) => {
     default:
       return { error: 'invalid resource name' };
   }
+  
+  // ...
+  
+  return { error: 'invalid request' };
+});
+
+// create a resource
+api.post({ resource: 'string' }, ({ token, resource }) => {
+  if(token === 'EMPTY' && resource === 'new token')
+  {
+    const token = generateToken();
+    Tokens.add(token);
+    Users.set(token, { clicks: 0, spaces: 0 });
+    return { token };
+  }
+  
+  if(Tokens.valid(token) === false)
+    return { 'invalid token' };
+  
+  // ...
+  
+  return { error: 'invalid request' };
 });
